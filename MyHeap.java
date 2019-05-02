e0c34091d7258f865b1fa6db17e8d4a40c6a77c1
Kayla Fang <kfang00@stuy.edu>
2019-05-02 02:00:38 -0400

    fixing errors

--- MyHeap.java
+++ MyHeap.java
@@ -7,7 +7,7 @@ public class MyHeap {
 - precondition: size is between 0 and data.length-1 inclusive. */
   private static void pushDown(int[]data,int size,int index) {
     int hold = 0;
-    if ((((index * 2) + 1) <= size) && (((index * 2) + 2) <= size)) {
+    if ((((index * 2) + 1) < size) && (((index * 2) + 2) < size)) {
       if ((data[(index * 2) + 1] > data[index]) || ((data[(index * 2) + 2] > data[index]) )) {
         if (data[(index * 2) + 1] > data[(index * 2) + 2]) {
           hold = data[index];
@@ -23,7 +23,7 @@ public class MyHeap {
         }
       }
     }
-    else if (((data[index] * 2) + 1) <= size) {
+    else if (((data[index] * 2) + 1) < size) {
       if (((data[index] * 2) + 1) > data[index]) {
           hold = data[index];
           data[index] = data[(index * 2) + 1];
@@ -39,9 +39,9 @@ public class MyHeap {
   private static void pushUp(int[]data,int index) {
     /*- push the element at index i up into the correct position. This will swap it with the parent node until the parent node is larger or the root is reached. [ should be O(logn) ]
     - precondition: index is between 0 and data.length-1 inclusive.*/
+    int hold = 0;
     if (index > 0) {
       if (data[(index - 1)/2] < data[index]) {
-        int hold = 0;
         hold = data[index];
         data[index] = data[(index - 1)/2];
         data[(index - 1)/2] = hold;
@@ -61,8 +61,8 @@ public class MyHeap {
 //We will discuss this today:
   public static void heapify(int[] data) {
       //- convert the array into a valid heap. [ should be O(n)
-      for (int a = (data.length - 1); a > 0; a--) {
-        pushUp(data, a);
+      for (int a = (data.length - 1); a >= 0; a--) {
+        pushDown(data, data.length, a);
       }
   }
 
@@ -79,10 +79,23 @@ public class MyHeap {
        data[0] = data[bound];
        data[bound] = hold;
        pushDown(data, bound, 0);
+       bound--;
      }
   }
 
+  public static String toString(int[] data) {
+    String h = "[";
+    for (int a = 0; a < data.length; a++) {
+      h += data[a] + ", ";
+    }
+    return h + "]";
+  }
+
   public static void main(String[] args){
+    int[] a = {0, 2, 6, 1300, 22,53,34,55,661,61,71,61,51};
+    heapsort(a);
+    System.out.println(toString(a));
+    HeapPrinter.print(a);
     System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
     int[]MAX_LIST = {1000000000,500,10};
     for(int MAX : MAX_LIST){
